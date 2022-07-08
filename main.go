74f5d9f45ea4a50c6c8a108181009bd532eb254a
hiendv02102000 <dovanhien021000@gmail.com>
2022-07-09 01:45:50 +0700

    1234

--- main.go
+++ main.go
@@ -5,13 +5,7 @@ import (
 )
 
 func main() {
-	// go func() {
-	// 	for i := 0; i < 1000; i++ {
-	// 		fmt.Println(i)
-	// 		time.Sleep(1000 * time.Millisecond)
-	// 	}
 
-	// }()
 	r := router.NewRouter()
 	r.Engine.Run()
 
